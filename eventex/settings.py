62740dc3d15204a9cfd3f822ab4e47e97d5fffc3
Dyesten <dyesten.pt@gmail.com>
2014-03-05 00:35:47 -0300

    'end'

--- eventex/settings.py
+++ eventex/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = (
 	'south',
     'eventex.core',
 	'eventex.subscriptions',	
+	'eventex.myauth',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -93,3 +94,12 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR.child('staticfiles')
 STATIC_URL = '/static/'
+
+'''
+AUTHENTICATION_BACKENDS = (
+	'eventex.myauth.backends.EmailBackend',
+	'django.contrib.auth.backends.ModelBackend',
+)
+'''
+
+AUTH_USER_MODEL = 'myauth.User'
